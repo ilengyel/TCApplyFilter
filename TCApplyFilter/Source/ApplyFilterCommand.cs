c0586330df5b2cdc3c155f90d91c4a9ea738ab1c
Imre Lengyel <ilengyel@gmail.com>
2017-06-04 10:03:20 +0200

    Add xml filtering functionality (#1)
    * Add xml filtering functionality
    
    * Out path
    
    * Force another build
    
    * Cant get analyzers to work with new format

---
+++ TCApplyFilter/Source/ApplyFilterCommand.cs
@@ -0,0 +1,35 @@
+﻿namespace TCApplyFilter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using CommandLine;
+
+    public class ApplyFilterCommand
+    {
+        [Option('m', "mds", Required = true, HelpText = "TestComplete test items list file.")]
+        public string MdsFile { get; set; }
+
+        [Option('t', "tags", HelpText = "List of tags to filter in. In OR mode.")]
+        public IEnumerable<string> Tags { get; set; }
+
+        [Option('o', "output", HelpText = "Output file for mutated mds file. Default is to update existing file inline.")]
+        public string OutputFile { get; set; }
+
+        public int Run()
+        {
+            try
+            {
+                var targetFile = OutputFile ?? MdsFile;
+                var filter = new TestCompleteFilter();
+                filter.FilterItems(MdsFile, targetFile, Tags.ToList());
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+                return 1;
+            }
+        }
+    }
+}
---
+++ TCApplyFilter/Source/IIncludeFilter.cs
@@ -0,0 +1,9 @@
+﻿namespace TCApplyFilter
+{
+    using System.Collections.Generic;
+
+    public interface IIncludeFilter<T>
+    {
+        void FilterItems(string input, string output, IList<T> includeItems);
+    }
+}
